2631bae5ad2ad620bf173fc1e10d6b0e202d8fb8
santhosh2410 <52484980+santhosh2410@users.noreply.github.com>
2021-03-21 13:27:55 +0530

    Add files via upload

--- Djant/settings.py
+++ Djant/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$mhgxp$cla+ii%n&%i)dss#_a429kw(wz$h-bukby_7h%q_sz+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['demoxite.herokuapp.com','127.0.0.1']
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,9 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT =[ 
-    os.path.join(BASE_DIR,'static')
-]
+STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
