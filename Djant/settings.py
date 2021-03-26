6a1b9f3c83ca588ca51c75e4aa4078159fad85cc
santhosh2410 <52484980+santhosh2410@users.noreply.github.com>
2021-03-26 10:21:56 +0530

    Add files via upload

--- Djant/settings.py
+++ Djant/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'Djant.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +125,10 @@ USE_TZ = True
 STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
+MEDIA_URL='/images/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
 ]
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
+
