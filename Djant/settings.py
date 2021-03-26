a7c20e583c34494ea389c6c279a6b903b9c3abe9
santhosh2410 <52484980+santhosh2410@users.noreply.github.com>
2021-03-26 10:27:21 +0530

    Update settings.py

--- Djant/settings.py
+++ Djant/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$mhgxp$cla+ii%n&%i)dss#_a429kw(wz$h-bukby_7h%q_sz+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['demoxite.herokuapp.com','127.0.0.1']
 
